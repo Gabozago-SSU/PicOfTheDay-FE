ff5da2f9e14397595ff02c06f16fadc523de9297
Hyomk <qet557@gmail.com>
2022-08-06 04:09:27 +0900

    feat : 후기 , 게시글 component

---
+++ src/components/PlaceDetail/index.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+
+import colors from "styles/colors";
+
+import * as S from "./styles";
+import ProfileImg from "../commons/ProfileImg/index";
+import StarIc from "../../assets/SmallStarIc.svg";
+import PlaceIc from "../../assets/PlaceIc.svg";
+import NextIc from "../../assets/PinkNextIc.svg";
+import KeywordChip from "../commons/Chip/KeywordChip";
+import HelpButton from "../commons/Button/HelpButton";
+
+import { PropTypes } from "prop-types";
+
+const PlaceDetail = ({ id, profile, nickName, rating, address, img, content, tags, helpNum }) => {
+    const [isLike, setLike] = useState(false);
+
+    const onClickHelpBtn = () => {
+        setLike(true);
+        setTimeout(() => {
+            setLike(false);
+        }, 500);
+    };
+
+    return (
+        <S.StyledLayout>
+            <S.InfoWrapper>
+                <ProfileImg img={profile}></ProfileImg>
+                <S.NameBox>{nickName}</S.NameBox>
+                <div style={{ height: "20px" }}>
+                    <img src={StarIc} style={{ width: "16px", alignSelf: "center" }}></img>
+                </div>
+                <S.RatingText>{parseFloat(rating).toFixed(1)}</S.RatingText>
+            </S.InfoWrapper>
+            {address ? (
+                <S.InfoWrapper style={{ marginTop: "9px" }}>
+                    <div style={{ height: "14px" }}>
+                        <img src={PlaceIc} style={{ height: "14px", alignSelf: "center" }}></img>
+                    </div>
+                    <S.AddressTextBox>
+                        <p>{address}</p>
+                    </S.AddressTextBox>
+                    <S.PhotoZonWrapper>포토존 보러가기</S.PhotoZonWrapper>
+                    <div style={{ height: "14px" }}>
+                        <img src={NextIc} style={{ width: "14px", alignSelf: "center" }}></img>
+                    </div>
+                </S.InfoWrapper>
+            ) : null}
+            <S.ImgBox img={img}>{isLike ? <S.HeartBox isLiked={isLike}></S.HeartBox> : null}</S.ImgBox>
+            <S.ContentBox>{content}</S.ContentBox>
+            {tags ? (
+                <S.TagWrapper>
+                    {tags.map((t, index) => (
+                        <KeywordChip key={index}>{t}</KeywordChip>
+                    ))}
+                </S.TagWrapper>
+            ) : null}
+            <S.HelpTextBox>{helpNum ? helpNum : 0}명에게 도움이 되었어요</S.HelpTextBox>
+
+            <div style={{ paddingLeft: "10px", paddingRight: "10px", paddingBottom: "40px" }}>
+                <HelpButton onClick={onClickHelpBtn}></HelpButton>
+            </div>
+        </S.StyledLayout>
+    );
+};
+
+export default PlaceDetail;
+PlaceDetail.propTypes = {
+    id: PropTypes.number,
+    profile: PropTypes.string,
+    nickName: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    address: PropTypes.string,
+    img: PropTypes.string,
+    content: PropTypes.string,
+    tags: PropTypes.array,
+    helpNum: PropTypes.number.isRequired,
+};
