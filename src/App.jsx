808d35eff9444373e7cab7f083218e57b02eca00
Hyomk <qet557@gmail.com>
2022-08-06 17:39:45 +0900

    feat : 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import GlobalStyle from "./styles/globalStyles";
 import { Routes, Route, Switch } from "react-router-dom";
 import { MediaDiv } from "./styles/common";
-import Login from './pages/Loginpage/Login';
-import Nikname from './pages/Niknamepage/Nikname';
+import Login from "./pages/Loginpage/Login";
+import Nikname from "./pages/Niknamepage/Nikname";
+
+import BottomNav from "./components/BottomNav";
+import HomePage from "pages/Home";
+import MapPage from "./pages/Map";
+import FeedPage from "./pages/Feed";
+import ReviewPage from "./pages/Review";
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
             <MediaDiv>
-                <Login />
-                {/* <Nikname /> */}
+                <Routes>
+                    <Route exact={true} path="*" element={<HomePage />}></Route>
+                    <Route exact={true} path="/map" element={<MapPage />}></Route>
+                    <Route exact={true} path="/feed" element={<FeedPage />}></Route>
+                    <Route exact={true} path="/review" element={<ReviewPage />}></Route>
+                    <Route exact={true} path="/mypage" element={<MapPage />}></Route>
+                    <Route exact={true} path="/login" element={<Login />}></Route>
+                    <Route exact={true} path="/signup" element={<Nikname />}></Route>
+                </Routes>
+                <BottomNav />
             </MediaDiv>
         </div>
     );
