fa53f1f98a77fd1e55e8c4de66003c4b2aef3f90
seocylucky <seocylucky@github.com>
2022-08-06 12:48:32 +0900

    로그인 및 회원가입 화면

---
+++ src/pages/Loginpage/LoginStyle.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import colors from '../../styles/colors';
+
+export const SlogunDiv = styled.div`
+    width: 300px;
+    height: 79px;
+    left: 37px;
+    top: 142px;
+    font-size: 24px;
+    line-height: 38px;
+    color: #0A0A0A;
+
+`;
+
+export const SlogunBold = styled.div`
+    float: left;
+    font-weight: 700;
+`;
+
+export const SlogunRow = styled.div`
+    margin-right: 8px;
+    float: left;
+`;
+
+export const FirstSlogun = styled.div`
+    padding-bottom: 6px;
+`;
+
+export const LoginButtonDiv = styled.div`
+    margin-top: 95px;
+`;
+
+export const LoginButton = styled.div`
+    margin-top: 21px;
+`;
+
+export const SignUpDiv = styled.div`
+    width: 134px;
+    height: 21px;
+    left: 120px;
+    top: 516px;
+
+    margin-top: 33px;
+    font-size: 12px;
+    line-height: 19px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    letter-spacing: -0.348029px;
+    margin-left: 83px;
+    margin-right: 83px;
+`;
+
+export const SignupButton = styled.div`
+    margin-left: 6px;
+    text-decoration-line: underline;
+    color: #FF3358;
+    font-weight: 700;
+`
+
+export const LoginDiv = styled.div`
+    padding-top: 142px;
+    padding-bottom: 275px;
+    margin: 0 auto;
+    background-color: white;
+
+    padding-left: 37px;
+    padding-right: 37px;
+`;
---
+++ src/pages/Niknamepage/NiknameStyle.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import colors from '../../styles/colors';
+
+export const NiknameDiv = styled.div`
+    padding-top: 142px;
+    margin: 0 auto;
+    position: relative;
+    width: 375px;
+    height: 812px;
+    padding-left: 27px;
+`;
+
+export const InputNikname = styled.div`
+
+    font-size: 24px;
+    line-height: 40px;
+    color: #0A0A0A;
+
+    width: 300px;
+    height: 45px;
+    left: 37px;
+    top: 142px;
+
+`;
+
+export const WriteNikname = styled.div`
+    background-color: ${colors.bgColor};
+
+    
+    width: 300px;
+    height: 39px;
+    left: 37px;
+    top: 189px;
+
+    line-height: 29px;
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.0015em;
+
+    color: #6C6C6C;
+    font-size: 20px;
+
+    border-bottom: 1px solid #B6B6B6;;
+
+    padding-top: 2px;
+    margin-bottom: 50px;
+`;
+
+export const OkNikname = styled.div`
+    padding-bottom: 488px;
+`;
