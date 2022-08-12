3217f135b75d390bd2307a7698398c6842c77cc6
Hyomk <qet557@gmail.com>
2022-08-13 04:07:49 +0900

    feat : 마이페이지 프로필

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { RecoilRoot } from "recoil";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GOOGLE_CLIENT_KEY } from "privateKey";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <RecoilRoot>
+                <GoogleOAuthProvider clientId={GOOGLE_CLIENT_KEY}>
+                    <App />
+                </GoogleOAuthProvider>
+            </RecoilRoot>
         </BrowserRouter>
     </React.StrictMode>,
 );
