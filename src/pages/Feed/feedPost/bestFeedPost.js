9b9f185c427203d5aedcddfcf794fc4805435d6a
seocylucky <seocylucky@github.com>
2022-08-17 23:27:17 +0900

    200 Error

--- src/pages/Feed/feedPost/bestFeedPost.js
+++ src/pages/Feed/feedPost/bestFeedPost.js
@@ -1,90 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import colors from "styles/colors";
 import { useNavigate } from "react-router-dom";
-
-export const feedArray1 = [
-    {
-        reviewid: 1,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 2,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 3,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 4,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 5,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 6,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 7,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 8,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 9,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 10,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 11,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 12,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 13,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 14,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 15,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 16,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 17,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 18,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 19,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-    {
-        reviewid: 20,
-        image: "https://cdn.class101.net/images/7b9546d0-3294-4839-b17b-0c5c9edfc587",
-    },
-];
+import { requestPopularFeed } from '../../../apis/index';
 
 export const StyleBox = styled.div`
     width: 100%;
@@ -104,18 +22,31 @@ export const PostlistBox = styled.div`
 
 function BestFeedPost() {
     const navigate = useNavigate();
+
+    const [popularFeed, setPopularFeed] = useState([]);
+
+    try{
+        requestPopularFeed().then((res) => {
+            setPopularFeed();
+            console.log(res);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
+
     return (
         <>
             <PostlistBox>
-                {feedArray1.map(({ reviewid, image }) => (
+                {popularFeed && popularFeed.map(({ reviewid, image }) => (
                     <StyleBox
                         key={reviewid}
                         image={image}
                         // 저기 id 에 1대신 리뷰아이디 넘겨주기!
                         onClick={() => {
-                            navigate("/feed/detail", { state: { id: 1 } });
+                            navigate("/feed/detail", { state: { id: reviewid } });
                         }}
-                    ></StyleBox>
+                    />
                 ))}
             </PostlistBox>
         </>
